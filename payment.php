095449988a0202c8d3926c43d41aa4ce6b5e4f66
kirtan-prajapati <kirtansprajapati@gmail.com>
2021-04-02 17:41:17 +0530

    paypal related code

---
+++ payment.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>payment</title>
+</head>
+<body>
+payment
+</body>
+</html>
---
+++ paypal.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>paypal</title>
+</head>
+<body>
+paypal
+</body>
+</html>
